e2a2e12c3f142d175cac882bf4cef090f033be33
rxseger <rseger@gmx.co.uk>
2016-10-09 22:54:45 +0000

    Use fifo-js to read events from motion

--- README.md
+++ README.md
@@ -4,12 +4,19 @@
 1.	Install Homebridge using `npm install -g homebridge`
 2.	Install this plugin `npm install -g homebridge-camera-motion`
 3.	Update your configuration file - see below for an example
+4.	Install and configure [Motion](https://motion-project.github.io)
 
-Connect the BME280 chip to the I2C bus
+Add to your `~/.motion/motion.conf`:
+
+```
+on_picture_save printf '%f\t%n\t%v\t%i\t%J\t%K\t%L\t%N\t%D\n' > /tmp/camera-pipe
+```
 
 ## Configuration
 * `accessory`: "CameraMotion"
 * `name`: descriptive name
+* `pipePath`: path to a [Unix named pipe](https://en.wikipedia.org/wiki/Named_pipe) to communicate with the camera
+(will be created if needed, must match output file pipe written to by Motion on_picture_save)
 
 Example configuration:
 
@@ -17,7 +24,8 @@ Example configuration:
     "accessories": [
         {
             "accessory": "CameraMotion",
-            "name": "Window"
+            "name": "Window",
+            "pipePath": "/tmp/camera-pipe"
         }
     ]
 ```
--- camera-motion.js
+++ camera-motion.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const FIFO = require('fifo-js');
+
 let Service, Characteristic;
 
 module.exports = (homebridge) => {
@@ -15,6 +17,10 @@ class CameraMotionPlugin
     this.log = log;
     this.name = config.name;
 
+    this.pipePath = config.pipePath || '/tmp/camera-pipe';
+    this.pipe = new FIFO(this.pipePath);
+    this.pipe.setReader(this.onPipeRead.bind(this));
+
     this.motionService = new Service.MotionSensor(this.name);
 
     this.motionService
@@ -24,9 +30,25 @@ class CameraMotionPlugin
     // TODO: change value on motion detected event
   }
 
-  getMotionDetected(cb) {
-    console.log('was motion detected? ');
-    cb(null, false); // TODO
+  onPipeRead(text) {
+    console.log(`got from pipe: |${text}|`);
+    // on_picture_save printf '%f\t%n\t%v\t%i\t%J\t%K\t%L\t%N\t%D\n' > /tmp/camera-pipe
+    // http://htmlpreview.github.io/?https://github.com/Motion-Project/motion/blob/master/motion_guide.html#conversion_specifiers
+    // %f filename with full path
+    // %n number indicating filetype
+    // %v event
+    // %i width of motion area
+    // %J height of motion area
+    // %K X coordinates of motion center
+    // %L Y coordinates of motion center
+    // %N noise level
+    // %D changed pixels
+    const [filename, filetype, event, width, height, x, y, noise, dpixels] = text.trim().split('\t');
+    console.log('filename is',filename);
+
+    this.motionService
+      .getCharacteristic(Characteristic.MotionDetected)
+      .setValue(true);
   }
 
   getServices() {
